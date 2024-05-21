eb82e192b7f104d5e8afdf746fc2f77c87b2aa1c
Uditya Kumar <74761401+rishi95122@users.noreply.github.com>
2024-05-21 15:06:26 +0530

    Update course.js

--- routes/course.js
+++ routes/course.js
@@ -4,7 +4,7 @@ import { add,get,addcourse,getcourses ,getcoursesBycategory,getallcourses} from 
 const router =express()
   
 router.post("/add", add);
-router.post("/get", get);
+router.post("/get",authenticate, get);
 router.post("/addcourse", addcourse);
 router.post("/getcourses", getcourses);
 router.post("/getcoursesBycategory", getcoursesBycategory);
