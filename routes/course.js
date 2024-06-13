641b8da632d90f1a5067e26f1306fe369f0939bc
uditya kumar <rishi95122@gmail.com>
2024-06-13 17:15:11 +0530

    12 junn"

--- routes/course.js
+++ routes/course.js
@@ -5,27 +5,27 @@ import cookieParser from "cookie-parser";
 const router =express()
   
 router.post("/add", add);
-router.post("/get", get);
+router.post("/get",authenticate, get);
 router.post("/addcourse", addcourse);
 router.post("/getcourses", getcourses);
 router.post("/getcoursesBycategory", getcoursesBycategory);
 router.get("/getallcourses", getallcourses);
 
 
-// function authenticate(req,res,next){
+function authenticate(req,res,next){
   
-//     const token =req.cookies['_ga']
-//     console.log("token",token)
-//     if(token==null) return res.sendStatus(401)
+    const token =req.cookies['access_token']
+    console.log("token",token)
+    if(token==null) return res.sendStatus(401)
        
-//     jwt.verify(token,"jwtkey",(err,user)=>{
-//       console.log(err,
-//         "fds",user)
-//         if(err) return res.sendStatus(403)
+    jwt.verify(token,"jwtkey",(err,user)=>{
+      console.log(err,
+        "fds",user)
+        if(err) return res.sendStatus(403)
        
-//      req.user=user
-//     next()
-//     })
-//     }
+     req.user=user
+    next()
+    })
+    }
 
 export default router; 
