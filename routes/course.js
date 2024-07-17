fc7a15a6d1fb95ab1dcbcc30417c2ccbffb205cf
uditya kumar <rishi95122@gmail.com>
2024-07-17 20:48:03 +0530

    new deleete

--- routes/course.js
+++ routes/course.js
@@ -6,7 +6,7 @@ import Cookies from "cookies";
 const router =express()
  
 router.post("/add", add);
-router.post("/get",authenticate, get);
+router.post("/get", get);
 router.post("/addcourse", addcourse);
 router.post("/getcourses", getcourses);
 router.post("/getcoursesBycategory", getcoursesBycategory);
@@ -15,7 +15,7 @@ router.post("/delete/:id", deleteCourse);
 router.post("/deletecoursecontent/:id", deleteCourseContent);
 
 
-function authenticate(req,res,next){
+function authenticsate(req,res,next){
     const cookies = new Cookies(req, res);
 
     const token =req.cookies['access_token']
